676f23fbcc83643ce86df74bdf38f45f94d02b43
Eric Matlock <ermatlock@gmail.com>
2021-09-25 18:26:38 -0400

    Add section 3 work

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,42 +24,54 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
+animals[2] = "Gorilla";
+console.log(animals);
 
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Tiger");
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.push("Elephant");
+console.log(animals);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = ['kale', 'onions', 'garlic', 'bell peppers'];
+console.log(foods);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.push('broccoli');
+console.log(foods)
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop();
+console.log(foods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+foods.push('asparagus', 'salmon', 'couscous');
+console.log(foods)
 
 // YOU DO: Remove the food that is in index position 0.
+foods.shift();
+console.log(foods)
+
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,12 +91,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Youtube history, which shows a list of videos you have previously watched.
+// 2: Instagram stories, which populates a list of recent stories posts.
+// 3: Sweetwater.com, which populates a list of synthesizers.
 
 
 //-------------------
@@ -96,16 +108,37 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
-The can call an uber if they have a charger and it is a car charger.
+They can call an uber if they have a charger and it is a car charger.
 
 */
 var percentBatteryLeft = 12;
 var hasCharger = true;
 var chargerType = "car";
+// var percentBatteryLeft = 15;
+// var hasCharger = false;
+// var chargerType = "car";
+// var percentBatteryLeft = 12;
+// var hasCharger = true;
+// var chargerType = "wall";
 
 // Write your conditional here
+// Create `if` conditional stating that percentBatteryLeft must be greater than or equal to 15.
+if (percentBatteryLeft >= 15) {
+// Return string "You can call an Uber"
+  console.log("You can call an Uber!");
+// create `else if` conditional using a logical operator stating that user must have a charger and that it must be a car charger.
+} else if ((hasCharger === true) && (chargerType === "car")) {
+// Return the string "You can call an Uber!"
+  console.log("You can call an Uber!");
+// Create `else` conditional as result if neither `if` nor `else if` conditions are met.
+} else {
+// Return the string, "Sorry, you are unable to call an uber at this time. Please charge your battery to at least 15%, or plug your phone into a car charger"
+  console.log("Sorry, you are unable to call an uber at this time. Please charge your battery to at least 15%, or plug your phone into a car charger")
+}
+
+
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -24,24 +24,34 @@ for (var i = 0; i < 3; i++) {
 
 // What is logged to the console for this second example? Why?
 // EXPLAIN:
+// For the second example the console is returning the incremental values of each `i` in the loop. This code is running the loop three times `i < 3` The initial value is 0, then in loop 2 the value is 1, then finally in loop three the value is 2.
 
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
+for (var i = 0; i < 7; i++) {
+  console.log(2 + 2);
+}
 
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
-
+for (var i = 0; i < 10; i++) {
+  console.log('She sells seashells down by the seashore');
+};
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
-
+for (var i = 1; i <= 10; i++) {
+  console.log("This is loop number " + (i));
+}
 // YOU DO: Using a for loop, how could you get an output that looks like this:
-
+for (var i = 10; i >= 1; i-=1) {
+  console.log(i);
+};
 // 10
 // 9
 // 8
@@ -53,6 +63,6 @@ for (var i = 0; i < 3; i++) {
 // 2
 // 1
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your pre-work group on Slack!
